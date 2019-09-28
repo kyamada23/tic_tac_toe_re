7a6a7b5d605478acab09f8e4c5e43d04886f3ff4
poty3 <poty.flute@gmail.com>
2019-09-28 13:23:09 +0900

    minorbug-fix

--- judge.php
+++ judge.php
@@ -15,6 +15,6 @@ if(isset($_POST['turn']) && isset($_POST['square_array'])){
   $result['next_name'] = $_SESSION[$next_turn]->getIconName();
   $result['dance'] = $_SESSION[$turn]->victoryDance();
   echo json_encode($result);
-  exit;
+  exit();
 }
 ?>
