a4513d9d3a8cff533cb5a1fd9bb564c792bcccdf
poty3 <poty.flute@gmail.com>
2019-09-29 00:34:17 +0900

    add_scss

--- board.php
+++ board.php
@@ -11,21 +11,36 @@ $data = $proc->getData();
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>tic tac toe</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="dist/style.css">
     <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
   </head>
   <body>
-    <section class="main">
-      <h1><?php echo $data['board']->getBoardName() ?></h1>
-      <div class="board-container">
-        <h2 class="js-message">開始！</h2>
-        <?php $data['board']->createBoard(); ?>
-      </div>
-      <form class="form-container" method="post">
-        <input type="submit" value="リセット" class="button" name="reset">
-        <input type="submit" value="TOPページに戻る" class="button" name="back">
-      </form>
+    <section id="main">
+      <section class="container">
+        <div class="row">
+          <div class="col">
+            <h1 class="page-title"><?php echo $data['board']->getBoardName() ?></h1>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <div class="board-container">
+              <h2 class="js-message">開始！</h2>
+              <?php $data['board']->createBoard(); ?>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <form class="form-container" method="post">
+              <input type="submit" value="リセット" class="button" name="reset">
+              <input type="submit" value="TOPページに戻る" class="button" name="back">
+            </form>
+          </div>
+        </div>
+      </section>
     </section>
 
     <script src="common/vendor/jquery-3.4.1.min.js"></script>
--- index.php
+++ index.php
@@ -11,34 +11,45 @@ $data = $proc->getData();
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>tic tac toe</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="dist/style.css">
     <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
   </head>
   <body>
-    <section class="main">
-      <h1>tic tac toe</h1>
-      <form class="form-container" method="post">
-        <h2>選んでください！</h2>
-        <ul>
-          <li>
-            <input type="radio" id="f-option" name="board-type" value="circle-cross" checked="checked">
-            <label for="f-option">Circle & Cross</label>
-            <div class="check"></div>
-          </li>
-          <li>
-            <input type="radio" id="s-option" name="board-type" value="dog-cat">
-            <label for="s-option">Dog & Cat</label>
-            <div class="check"><div class="inside"></div></div>
-          </li>
-          <li>
-            <input type="radio" id="t-option" name="board-type" value="bomb-burn">
-            <label for="t-option">Bomb & Burn</label>
-            <div class="check"><div class="inside"></div></div>
-          </li>
-        </ul>
-        <input type="submit" value="開始" class="button">
-      </form>
+    <section id="main">
+      <section class="container page-top">
+        <div class="row">
+          <div class="col">
+            <h1 class="page-top__title">tic tac toe</h1>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <form class="c-form" method="post">
+              <h2 class="c-form__title">選んでください！</h2>
+              <ul>
+                <li class="c-radio">
+                  <input type="radio" id="f-option" name="board-type" value="circle-cross" checked="checked">
+                  <label class="c-radio__label" for="f-option">Circle & Cross</label>
+                  <div class="check"></div>
+                </li>
+                <li class="c-radio">
+                  <input type="radio" id="s-option" name="board-type" value="dog-cat">
+                  <label class="c-radio__label" for="s-option">Dog & Cat</label>
+                  <div class="check"><div class="inside"></div></div>
+                </li>
+                <li class="c-radio">
+                  <input type="radio" id="t-option" name="board-type" value="bomb-burn">
+                  <label class="c-radio__label" for="t-option">Bomb & Burn</label>
+                  <div class="check"><div class="inside"></div></div>
+                </li>
+              </ul>
+              <input type="submit" value="開始" class="c-button">
+            </form>
+          </div>
+        </div>
+      </section>
     </section>
   </body>
 </html>
